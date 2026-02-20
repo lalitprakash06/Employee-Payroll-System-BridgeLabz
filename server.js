18808bce1175e8ea171b3cfd21800541f2c28606
lalitprakash06 <lalitprakash006@gmail.com>
2026-02-20 14:21:35 +0530

    Completed Project

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const fileHandler = require('./modules/fileHandler'); 
+const fileHandler = require('./modules/fileHandler');
 
 const app = express();
 
